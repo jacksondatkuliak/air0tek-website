a347ff0c35ce93120a91389ff273269cd9a9db9c
Jackson Datkuliak <22jdatku@goarrows.org>
2021-11-08 10:31:44 -0500

    .

---
+++ scripts/cloudMover.js
@@ -0,0 +1,12 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+        
+        function scrollFunction() {
+          if (document.body.scrollTop > 50  || document.documentElement.scrollTop > 50) {
+            document.getElementById("cloud").style.height = "65px";
+            document.getElementById("cloud").style.width = "187.5px";
+          } else {
+            document.getElementById("cloud").style.height = "260px";
+            document.getElementById("cloud").style.width = "750px";
+          }
+        }
