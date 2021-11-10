733bab28ec8094be1df34759a7c0f99ed397c4c4
22kreyno@goarrows.org <22kreyno@goarrows.org>
2021-11-10 13:28:27 -0500

    fixed header

--- scripts/cloudMover.js
+++ scripts/cloudMover.js
@@ -5,8 +5,12 @@ window.onscroll = function() {scrollFunction()};
           if (document.body.scrollTop > 50  || document.documentElement.scrollTop > 50) {
             document.getElementById("cloud").style.height = "65px";
             document.getElementById("cloud").style.width = "187.5px";
+            document.getElementById("topheader").style.paddingTop = "5px";
+            document.getElementById("topheader").style.paddingBottom = "5px";
           } else {
             document.getElementById("cloud").style.height = "260px";
             document.getElementById("cloud").style.width = "750px";
+            document.getElementById("topheader").style.paddingTop = "10px";
+            document.getElementById("topheader").style.paddingBottom = "10px";
           }
         }
