c300ab2f35b581835b8617e7b2352f97eba6f547
Jackson Datkuliak <jumpingjack1867@gmail.com>
2021-12-14 19:40:58 -0500

    Added an instagram embed to door.html and made the page automatically refresh every hour

---
+++ scripts/autoRefresh.js
@@ -0,0 +1,14 @@
+// wowee a js file that was actually written by one of the students!!!!!
+// This javascript file is included in door.html to refresh the page automatically every hour. It does this so the weather and instagram embeds get refreshed. Could I do this in another, none-instrusive way? Probably, but I'm lazy -Jackson D
+function refreshTimeout() {
+    var refresh = 3600000; //Time it will take to refresh the page, in milliseconds ; 3600000 = 1 hour
+    mytime=setTimeout('reload()', refresh);
+}
+
+function reload() {
+    location.reload();
+    console.log("is this script running?")
+    refreshTimeout();
+}
+
+refreshTimeout();
